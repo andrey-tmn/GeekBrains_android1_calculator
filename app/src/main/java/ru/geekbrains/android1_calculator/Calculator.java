44276547e9c6af96fc744e2bdb828761f2a82301
Andrey Nikolaevich <andrey.tmn.mail@gmail.com>
2022-01-27 20:14:22 +0500

    Повесил обработчики клика на кнопки операций

--- app/src/main/java/ru/geekbrains/android1_calculator/Calculator.java
+++ app/src/main/java/ru/geekbrains/android1_calculator/Calculator.java
@@ -3,10 +3,47 @@ package ru.geekbrains.android1_calculator;
 import java.io.Serializable;
 
 public class Calculator implements Serializable {
-    private int showingValue = 0;
-    private int previousValue = 0;
+    private String showingValue = "";
+    private double previousValue = 0;
 
-    public String addSymbol(){
-        return Integer.toString(showingValue);
+    public String addSymbol() {
+        return showingValue;
+    }
+
+    public String getValue() {
+        return showingValue;
+    }
+
+    public void reset() {
+        showingValue = "";
+        previousValue = 0;
+    }
+
+    public void backspace() {
+        //TODO
+    }
+
+    public void result() {
+        //TODO
+    }
+
+    public void decimal_separator() {
+        //TODO
+    }
+
+    public void addition() {
+        //TODO
+    }
+
+    public void subtraction() {
+        //TODO
+    }
+
+    public void division() {
+        //TODO
+    }
+
+    public void multiplication() {
+        //TODO
     }
 }
--- app/src/main/java/ru/geekbrains/android1_calculator/MainActivity.java
+++ app/src/main/java/ru/geekbrains/android1_calculator/MainActivity.java
@@ -1,13 +1,15 @@
 package ru.geekbrains.android1_calculator;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-import android.util.Log;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
     private Calculator calculator;
-    static private final String CALCULATOR_IN_BUNDLE = "CALCULATOR_IN_BUNDLE";
+    private TextView resultTextView;
+    private static final String CALCULATOR_IN_BUNDLE = "CALCULATOR_IN_BUNDLE";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -19,5 +21,55 @@ public class MainActivity extends AppCompatActivity {
         else
             calculator = (Calculator) savedInstanceState.getSerializable("CALCULATOR_IN_BUNDLE");
 
+        resultTextView = findViewById(R.id.textview_result);
+        resultTextView.setText(calculator.getValue());
+
+        findViewById(R.id.button_operation_reset).setOnClickListener(view -> doOperation("reset"));
+        findViewById(R.id.button_operation_backspace).setOnClickListener(view -> doOperation("backspace"));
+        findViewById(R.id.button_operation_result).setOnClickListener(view -> doOperation("result"));
+        findViewById(R.id.button_operation_decimal_separator).setOnClickListener(view -> doOperation("decimal_separator"));
+        findViewById(R.id.button_operation_addition).setOnClickListener(view -> doOperation("addition"));
+        findViewById(R.id.button_operation_subtraction).setOnClickListener(view -> doOperation("subtraction"));
+        findViewById(R.id.button_operation_division).setOnClickListener(view -> doOperation("division"));
+        findViewById(R.id.button_operation_multiplication).setOnClickListener(view -> doOperation("multiplication"));
+
+        
+    }
+
+    private void doOperation(final String operation) {
+        switch (operation) {
+            case "reset":
+                calculator.reset();
+                break;
+            case "backspace":
+                calculator.backspace();
+                break;
+            case "result":
+                calculator.result();
+                break;
+            case "decimal_separator":
+                calculator.decimal_separator();
+                break;
+            case "addition":
+                calculator.addition();
+                break;
+            case "subtraction":
+                calculator.subtraction();
+                break;
+            case "division":
+                calculator.division();
+                break;
+            case "multiplication":
+                calculator.multiplication();
+                break;
+        }
+        resultTextView.setText(calculator.getValue());
+    }
+
+    @Override
+    protected void onSaveInstanceState(@NonNull Bundle outState) {
+        super.onSaveInstanceState(outState);
+
+        outState.putSerializable(CALCULATOR_IN_BUNDLE, calculator);
     }
 }
